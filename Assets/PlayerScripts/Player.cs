e36709943fd521ecfa6c37f00f66686c9fd8bef5
Sonny Chaudhry <80045438+dani142c@users.noreply.github.com>
2024-10-11 13:37:10 +0200

    dor

--- Assets/PlayerScripts/Player.cs
+++ Assets/PlayerScripts/Player.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class Player : MonoBehaviour
 {
@@ -15,17 +17,22 @@ public class Player : MonoBehaviour
     public float speed = 1f;
     public float damage = 1f;
     public float health = 50f; 
+    public TextMeshProUGUI healthText;
+    public Canvas gameOver;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         instance = this;
+        healthText.text = health.ToString();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.R))
         {
             equippedGun.Reload();
@@ -50,6 +57,7 @@ public class Player : MonoBehaviour
 
     public void PlayerTakeDamage(float dmg){
         health -= dmg;
+        healthText.text = health.ToString();
 
         if(health <= 0){
             PlayerDeath();
@@ -59,5 +67,13 @@ public class Player : MonoBehaviour
     public void PlayerDeath(){
         SoundManager.instance.playSound(playerDeath, this.transform, 1f);
         //end game and add sfx
+        GameOver();
+    }
+
+    public void GameOver()
+    {
+        WaveManager.IsGamePaused = true;
+        Time.timeScale = 0;
+        gameOver.gameObject.SetActive(true);
     }
 }
